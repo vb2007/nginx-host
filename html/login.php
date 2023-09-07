bcf8acdd7779443f7d70907b70944b2302b87801
TheEvilApple <vagvolgyi.balazs.vb@gmail.com>
2023-09-07 15:52:29 +0200

    login

--- html/login.php
+++ html/login.php
@@ -3,7 +3,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $db = new SQLite3('login.db');
+    $db = new SQLite3('users.db');
+    if (!$db) {
+        die("Database connection failed: " . $db->lastErrorMsg());
+    }
 
     $query = $db->prepare("SELECT * FROM users WHERE username = :username");
     $query->bindParam(':username', $username);
@@ -11,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $user = $result->fetchArray(SQLITE3_ASSOC);
 
-    if ($user && password_verify($password, $user['password'])) {
+    if ($user && $password === $user['password']) {
         header("Location: welcome.html");
         exit();
     } else {
