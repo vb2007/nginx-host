f22fa1eb6fe3a6bbb5f5e04904d14be45ee37710
TheEvilApple <vagvolgyi.balazs.vb@gmail.com>
2023-09-23 11:26:54 +0200

    upload handler complete

--- html/asset/js/upload_handler.js
+++ html/asset/js/upload_handler.js
@@ -10,9 +10,32 @@ fileInput.addEventListener("change", (e) => {
 
 function uploadFile(File) {
     const chunkSize = 1024 * 1024; //1mb-os méretenként tölti fel a fájlt
-    let offset = 0;
+    let offset = 0; //ez a változó figyeli, hogy jelenleg hogy halad a fájl feltöltése
 
     const uploadChunk = () => {
         const chunk = file.slice(offset, offset + chunkSize);
-    }
+        const formData = new FormData();
+        formData.append("fileToUpload", chunk);
+        
+        fetch("upload.php?offset=" + offset, {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            offset += chunkSize;
+            if (offset < file.size) {
+                uploadChunk();
+            }
+            else{
+                alert("CHILD PORN UPLOADED SUCCESSFULLY");
+            }
+        })
+        .catch(error => {
+            console.error(":( CP upload error: ", error);
+            alert("No more child porn (error) :(");
+        });
+    };
+
+    uploadChunk();
 }
