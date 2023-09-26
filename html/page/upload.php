62eb0981705ee546f565063e082b3200a84c1a19
TheEvilApple <vagvolgyi.balazs.vb@gmail.com>
2023-09-26 18:17:18 +0200

    no more chunks

--- html/page/upload.php
+++ html/page/upload.php
@@ -3,72 +3,50 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-//   exit('POST request method required.');
-// }
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+  exit('POST request method required.');
+}
 
-// if (empty($_FILES)) {
-//   exit('$_FILES is empty - is file_uploads set to "Off" in php.ini?');
-// }
+if (empty($_FILES)) {
+  exit('$_FILES is empty - is file_uploads set to "Off" in php.ini?');
+}
 
-// if ($_FILES["fileToUpload"]["error"] !== UPLOAD_ERR_OK) {
-//   switch ($_FILES["fileToUpload"]["error"]) {
-//     case UPLOAD_ERR_PARTIAL:
-//         exit('File only partially uploaded');
-//         break;
-//     case UPLOAD_ERR_NO_FILE:
-//         exit('No file was uploaded');
-//         break;
-//     case UPLOAD_ERR_EXTENSION:
-//         exit('File upload stopped by a PHP extension');
-//         break;
-//     case UPLOAD_ERR_FORM_SIZE:
-//         exit('File exceeds MAX_FILE_SIZE in the HTML form');
-//         break;
-//     case UPLOAD_ERR_INI_SIZE:
-//         exit('File exceeds upload_max_filesize in php.ini');
-//         break;
-//     case UPLOAD_ERR_NO_TMP_DIR:
-//         exit('Temporary folder not found');
-//         break;
-//     case UPLOAD_ERR_CANT_WRITE:
-//         exit('Failed to write file');
-//         break;
-//     default:
-//         exit('Unknown upload error');
-//         break;
-//   }
-// }
+if ($_FILES["fileToUpload"]["error"] !== UPLOAD_ERR_OK) {
+  switch ($_FILES["fileToUpload"]["error"]) {
+    case UPLOAD_ERR_PARTIAL:
+        exit('File only partially uploaded');
+        break;
+    case UPLOAD_ERR_NO_FILE:
+        exit('No file was uploaded');
+        break;
+    case UPLOAD_ERR_EXTENSION:
+        exit('File upload stopped by a PHP extension');
+        break;
+    case UPLOAD_ERR_FORM_SIZE:
+        exit('File exceeds MAX_FILE_SIZE in the HTML form');
+        break;
+    case UPLOAD_ERR_INI_SIZE:
+        exit('File exceeds upload_max_filesize in php.ini');
+        break;
+    case UPLOAD_ERR_NO_TMP_DIR:
+        exit('Temporary folder not found');
+        break;
+    case UPLOAD_ERR_CANT_WRITE:
+        exit('Failed to write file');
+        break;
+    default:
+        exit('Unknown upload error');
+        break;
+  }
+}
 
 $target_dir = "/var/www/html/html/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$chunkNumber = $_SERVER['HTTP_X_CHUNK_NUMBER'];
-$totalChunks = $_SERVER['HTTP_X_TOTAL_CHUNKS'];
 
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file . '.part' . $chunkNumber)) {
-  echo "The chunk ". $chunkNumber . " has been uploaded.";
+if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+  echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 }
 else {
-  echo "Sorry, there was an error uploading your chunk.";
-}
-
-//Megnézi hogy fel van-e töltve az összes chunk
-$allChunksUploaded = true;
-for ($i = 0; $i < $totalChunks; $i++) {
-    if (!file_exists($target_file . '.part' . $i)) {
-        $allChunksUploaded = false;
-        break;
-    }
-}
-
-//Miután az összes chunk feltöltése megtörtént...
-if ($allChunksUploaded) {
-    echo "\nChunkok összeállítása...";
-    //Chunkok összeállítása
-    for ($i = 0; $i < $totalChunks; $i++) {
-        $data = file_get_contents($target_file . '.part' . $i);
-        file_put_contents($target_file, $data, FILE_APPEND);
-    }
-    echo "\nChunkok összeállítva!";
+  echo "Sorry, there was an error uploading your file.";
 }
 ?>
