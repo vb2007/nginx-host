566a570522cf1a01fece6a714658a1259cdc54a3
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2023-10-04 15:48:44 +0200

    13 orai vazlat

--- html/page/login_handler.php
+++ html/page/login_handler.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // header("Location: welcome.html");
         exit();
     } else {
-        echo "INVALID CREDENTIALS.\nTry bruteforcing, you will never get inside motherfucker ;)";
+        echo "INVALID CREDENTIALS.\n";
     }
 
     $db->close();
