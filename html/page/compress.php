e062ef9376d5ee07c5e9353299c45f62dee7be34
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2023-12-01 23:07:31 +0000

    mod

---
+++ html/page/compress.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="page/_script/compress.php" method="post" enctype="multipart/form-data">
+  Select file to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br>
+  Enter desired file size (in MB):
+  <input type="text" name="desiredSize" id="desiredSize">
+  <br>
+  <input type="submit" value="Upload File" name="submit">
+</form>
+
+</body>
+</html>
