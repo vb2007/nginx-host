739aa8091a3906f5a3bbf85032f6f80daee6309c
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-02-15 21:19:51 +0100

    un-tab

--- html/page/_script/upload.php
+++ html/page/_script/upload.php
@@ -16,29 +16,29 @@ if (empty($_FILES)) {
 if ($_FILES["fileToUpload"]["error"] !== UPLOAD_ERR_OK) {
   switch ($_FILES["fileToUpload"]["error"]) {
     case UPLOAD_ERR_PARTIAL:
-        exit('File only partially uploaded');
-        break;
+      exit('File only partially uploaded');
+      break;
     case UPLOAD_ERR_NO_FILE:
-        exit('No file was uploaded');
-        break;
+      exit('No file was uploaded');
+      break;
     case UPLOAD_ERR_EXTENSION:
-        exit('File upload stopped by a PHP extension');
-        break;
+      exit('File upload stopped by a PHP extension');
+      break;
     case UPLOAD_ERR_FORM_SIZE:
-        exit('File exceeds MAX_FILE_SIZE in the HTML form');
-        break;
+      exit('File exceeds MAX_FILE_SIZE in the HTML form');
+      break;
     case UPLOAD_ERR_INI_SIZE:
-        exit('File exceeds upload_max_filesize in php.ini');
-        break;
+      exit('File exceeds upload_max_filesize in php.ini');
+      break;
     case UPLOAD_ERR_NO_TMP_DIR:
-        exit('Temporary folder not found');
-        break;
+      exit('Temporary folder not found');
+      break;
     case UPLOAD_ERR_CANT_WRITE:
-        exit('Failed to write file');
-        break;
+      exit('Failed to write file');
+      break;
     default:
-        exit('Unknown upload error');
-        break;
+      exit('Unknown upload error');
+      break;
   }
 }
 
