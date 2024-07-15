8ce12b7f43fbd13d9504606d6f94015dcae3d1fe
vb2007 <vagvolgyi.balazs.vb@gmail.com>
2024-07-15 21:00:20 +0200

    updated more css referrals to cdn links

--- html/page/pastebin.php
+++ html/page/pastebin.php
@@ -13,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!--Local import-->
     <link rel="stylesheet" href="https://cdn.vb2007.hu/webstatic/css/design.css">
-    <link rel="stylesheet" href="../asset/css/pastebin.css">
+    <link rel="stylesheet" href="https://cdn.vb2007.hu/webstatic/css/pastebin.css">
     <!--Title-->
     <title>Paste text</title>
 </head>
